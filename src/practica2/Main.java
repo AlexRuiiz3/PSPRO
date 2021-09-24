624a9af57208d54a0d7e6d04cf745bd744516ba3
Alejandro Ruiz Campos <alexruiz29del12@gmail.com>
2021-09-24 14:32:22 +0200

    Clase 24/09/2021

--- src/practica2/Main.java
+++ src/practica2/Main.java
@@ -1,10 +1,39 @@
 package practica2;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+//Un proceso abrir bloc de nota y tienes un maxido de 30 segundos para crear un txt hola y ver si existe o no 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ProcessBuilder procesoBuilder = new ProcessBuilder("notepad");
+		Process proceso = null;
+		
 
+			try {
+				proceso = procesoBuilder.start(); //Se inicia el proceso
+				
+				proceso.waitFor(10, TimeUnit.SECONDS); //Se espera el tiempo que indique
+				
+				File file = new File("C:\\Users\\aruiz\\Documents\\GitHub\\PSPRO\\Hola2.txt");
+				
+				if(file.exists()) {
+					System.out.println("Existe el fichero");
+				}else {
+					System.out.println("No existe el fichero2");
+				}
+				
+				proceso.destroy(); //Sirve para terminar el processo y que en este caso se cierre el bloc de notas 
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} 
+		
 	}
 
 }
---
+++ src/practica3/Main.java
@@ -0,0 +1,31 @@
+package practica3;
+
+import java.io.IOException;
+import java.util.Map;
+
+
+//Un proceso abrir bloc de nota y llamar a enciroment que devuelve un map y mostrar lo que contiene
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ProcessBuilder procesoBuilder = new ProcessBuilder("notepad");
+		Process proceso = null;
+		
+
+			try {
+				proceso = procesoBuilder.start(); //Se inicia el proceso
+				
+				Map<String, String> mapa = procesoBuilder.environment();//Devuelve una vista de mapa de cadenas del entorno de este generador de procesos
+				
+				//Con un for each se muestra un mapa
+				for(Map.Entry<String, String> entry : mapa.entrySet()) {
+					System.out.println("Clave: "+entry.getKey()+" 		--- Valor: "+entry.getValue());
+				}
+				
+				proceso.destroy(); //Sirve para terminar el processo y que en este caso se cierre el bloc de notas 
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+}
